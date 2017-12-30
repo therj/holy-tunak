78ca64e77f6a6b110e9fe869e96fe50038839061
Rabindra Joshi <therj@users.noreply.github.com>
2017-12-30 20:57:06 +0545

    The Holy Tunak: Play Now

---
+++ app/src/main/java/tuank/holy/MainActivity.java
@@ -0,0 +1,26 @@
+package tuank.holy;
+
+import android.media.MediaPlayer;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        final MediaPlayer tunakSound = MediaPlayer.create(this, R.raw.tunak);
+        Button playNow = (Button) this.findViewById(R.id.play_holy_tunak);
+        playNow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //tunakSound.setLooping(true);
+                tunakSound.start();
+
+            }
+        });
+    }
+}
